9b1aeac383328274bf7a468f9c3f28506bf4c8b5
Student <student@macbook-pro-415-hodgson.local>
2016-12-06 10:49:33 -0500

    I had problems getting my black background working

--- ISPHodgsonRGame/GameViewController.swift
+++ ISPHodgsonRGame/GameViewController.swift
@@ -8,48 +8,25 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let scene = GameScene (size: CGSize(width: 2048, height: 1536)
+            
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+        
     }
-
-    override var shouldAutorotate: Bool {
+           override var prefersStatusBarHidden: Bool {
         return true
     }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
+        override var prefersStatusBarHidden: Bool {
+            return true
 }
