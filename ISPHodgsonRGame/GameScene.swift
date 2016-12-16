12fc7e9e68a5ef4ff546a1e55b3aac049e19a236
Student <student@macbook-pro-415-hodgson.local>
2016-12-16 09:49:18 -0500

    I made a function for the movement of my banana and I now i am working on implementing a touch function.

--- ISPHodgsonRGame/GameScene.swift
+++ ISPHodgsonRGame/GameScene.swift
@@ -8,10 +8,10 @@
 
 import SpriteKit
 
-class GameScene : SKScene {
+    class GameScene : SKScene {
     
     let monkey = SKSpriteNode(imageNamed: "Monkey")
-    
+        
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
         let background = SKSpriteNode(imageNamed: "Jungle")
@@ -24,6 +24,8 @@ class GameScene : SKScene {
         monkey.position = CGPoint(x: size.width / 2, y: 200)
         monkey.setScale(3.3)
         addChild(monkey)
+        
+    
         //Periodically Spawn Obstacles
         let actionWait = SKAction.wait(forDuration: 2)
         let actionSpawn = SKAction.run() { [weak self] in self?.spawnObstacle() }
@@ -35,9 +37,7 @@ class GameScene : SKScene {
     func spawnObstacle() {
         let obstacle = SKSpriteNode(imageNamed: "target")
         
-            obstacle.setScale(0.2)
-        
-        let verticalPosition = CGFloat(arc4random_uniform(UInt32(size.height)))
+        let verticalPosition = CGFloat(arc4random_uniform(UInt32(size.height - 350))) + 350
         let horizontalPosition = size.width + obstacle.size.width
         let startingPosition = CGPoint(x: horizontalPosition, y: verticalPosition)
         
@@ -46,31 +46,59 @@ class GameScene : SKScene {
         addChild(obstacle)
         
         let endingPosition = CGPoint(x: -100, y: verticalPosition )
-        let actionMove = SKAction.move(to: endingPosition, duration: 5)
+        let actionMove = SKAction.move(to: endingPosition, duration: 4.2)
         
         
         let actionRemove = SKAction.removeFromParent()
         
         let actionSequence = SKAction.sequence([actionMove, actionRemove])
         
-    
+        
         obstacle.run(actionSequence)
-    }
+        obstacle.setScale(0.2)
+        
+        }
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+            guard let touch = touches.first else {
+                return
+            }
+            let touchLocation = touch.location(in: self)
+            
+            print(touchLocation.x)
+            print(touchLocation.y)
+            
+            let destination = CGPoint (x: touchLocation.x, y: monkey.position.y)
+            
+            
+        }
+
+        func shootProjectile() {
+    
+            //creating an instance
+            let projectile = SKSpriteNode(imageNamed: "banana")
+    
+            // define starting positon
+        let startingPosition =  CGPoint (x: monkey.position.x, y: monkey.position.y)
+        projectile.position = startingPosition
+        projectile.setScale(0.2)
+        projectile.zPosition = 3
+        
+        
+            //add the obstacle to the scene
+            addChild(projectile)
+            
+            //Move the banana
+        let endingPosition = CGPoint(x: monkey.position.x, y: 1000 )
+        let projectileMove = SKAction.move(to: endingPosition, duration: 5)
+        projectile.run(projectileMove)
+        }
+       
+    
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-    
-    
 
 
 
