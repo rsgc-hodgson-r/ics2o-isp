3931e1b00f8d7722fea721eaaf2bb77c99911ece
Student <student@macbook-pro-415-hodgson.local>
2017-01-03 14:54:31 -0500

    I got my game to detect collision so now when a banana hits a target they both disappear.

--- ISPHodgsonRGame/GameScene.swift
+++ ISPHodgsonRGame/GameScene.swift
@@ -8,10 +8,9 @@
 
 import SpriteKit
 
-    class GameScene : SKScene {
+class GameScene : SKScene {
     
     let monkey = SKSpriteNode(imageNamed: "Monkey")
-        
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
         let background = SKSpriteNode(imageNamed: "Jungle")
@@ -25,7 +24,7 @@ import SpriteKit
         monkey.setScale(3.3)
         addChild(monkey)
         
-    
+        
         //Periodically Spawn Obstacles
         let actionWait = SKAction.wait(forDuration: 2)
         let actionSpawn = SKAction.run() { [weak self] in self?.spawnObstacle() }
@@ -34,8 +33,61 @@ import SpriteKit
         run(actionObstacleRepeat)
     }
     
+    // This method runs about 60 times per second (60 fps)
+    override func update(_ currentTime: TimeInterval) {
+        
+        // check for collisions between the bananas and the targets
+        checkAllBananas()
+    }
+    
+    //find all bananas currently on the screen
+    
+    func checkAllBananas() {
+        
+    // find all bananas currently in flight. 
+        enumerateChildNodes(withName: "banana", using: {
+                node, _ in
+            
+    // get a reference to the current banana
+            let banana = node as! SKSpriteNode
+            
+    // using this single  banana, check  for collisions against all targets on screen currently. 
+            
+            self.lookForTargetsHitBy(banana)
+            
+        })
+        
+    }
+    // Check for Collisions between a given target and a banana
+    func lookForTargetsHitBy(_ banana : SKSpriteNode) {
+    
+    //find all targets currently in flight
+    enumerateChildNodes(withName: "target", using: {
+        node, _ in
+            
+    //get a reference to the current target
+    
+        let target =  node as! SKSpriteNode
+    
+    //determine wether this target is being hit by a banana
+    
+        if target.frame.insetBy(dx: 20, dy:
+            20).intersects(banana.frame.insetBy(dx: 20, dy: 20)) {
+        
+    // There is a collision so remove both nodes
+    banana.removeFromParent()
+    target.removeFromParent()
+            
+            }
+        
+        })
+    }
+        
+        
+        
     func spawnObstacle() {
         let obstacle = SKSpriteNode(imageNamed: "target")
+        obstacle.name = "target"
         
         let verticalPosition = CGFloat(arc4random_uniform(UInt32(size.height - 350))) + 350
         let horizontalPosition = size.width + obstacle.size.width
@@ -57,68 +109,65 @@ import SpriteKit
         obstacle.run(actionSequence)
         obstacle.setScale(0.2)
         
+    }
+    //method responds to a single touch
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // when we touch a device are fingers are big
+        guard let touch = touches.first else {
+            return
         }
-        //method responds to a single touch
-        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-           
-            // when we touch a device are fingers are big
-            guard let touch = touches.first else {
-                return
-            }
-            //Print the location of the first touch
-            let touchLocation = touch.location(in: self)
-            
-           //print the location of the touch in the console
-            print(touchLocation.x)
-            print(touchLocation.y)
-            
-            //let monkey move horizontally towards the touc.
-            let destination = CGPoint (x: touchLocation.x, y: monkey.position.y)
-            
-            //create action 
-            let actionMove = SKAction.move(to: destination, duration: 5)
-            
-            // tell the monkey sprite to move
-            monkey.run(actionMove)
-            
-            shootProjectile()
-        }
-
-        func shootProjectile() {
+        //Print the location of the first touch
+        let touchLocation = touch.location(in: self)
+        
+        //print the location of the touch in the console
+        print(touchLocation.x)
+        print(touchLocation.y)
+        
+        //let monkey move horizontally towards the touch.
+        let destination = CGPoint (x: touchLocation.x, y: monkey.position.y)
+        
+        //create action
+        let actionMove = SKAction.move(to: destination, duration: 5)
+        
+        // tell the monkey sprite to move
+        monkey.run(actionMove)
+        
+        shootProjectile()
+    }
     
-            //creating an instance
-            let projectile = SKSpriteNode(imageNamed: "fruit")
-    
-            // define starting positon
+    func shootProjectile() {
+        
+        //creating an instance
+        let projectile = SKSpriteNode(imageNamed: "banana")
+        
+        // define starting positon
         let startingPosition =  CGPoint (x: monkey.position.x, y: monkey.position.y)
         projectile.position = startingPosition
-        projectile.setScale(0.3)
+        projectile.setScale(0.1)
+        projectile.name = "banana"
         projectile.zPosition = 3
         
         
-        //add the obstacle to the scene
+        //add the projectile to the scene
         addChild(projectile)
-            
+        
         //Move the banana
         let endingPosition = CGPoint(x: monkey.position.x, y: 2000)
         let projectileMove = SKAction.move(to: endingPosition, duration: 2)
         
         let actionRemove = SKAction.removeFromParent() //This will remove the banana from the scene graph of the game
-            
+        
         // this defines the sequence of what should happen for this node
         let actionSequence = SKAction.sequence([projectileMove, actionRemove])
         
-            
+        
         
         //now instead of running a single action we run the sequence.
         projectile.run(actionSequence)
-        }
-       
+    }
     
-
 }
-
-
 
 
 
